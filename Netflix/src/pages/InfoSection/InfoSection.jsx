885f25fe2a0e7c9e205d993a22f57bbff22b82c1
Hemanth-Krishnamoorthy <kkhemanth.skilldev@gmail.com>
2025-11-11 11:29:44 +0530

    added JSON

--- Netflix/src/pages/InfoSection/InfoSection.jsx
+++ Netflix/src/pages/InfoSection/InfoSection.jsx
@@ -1,40 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import FeatureCard from "../../components/FeatureCard/FeatureCard";
 import "./InfoSection.css";
 
 export default function InfoSection() {
-  const features = [
-    {
-      id: 1,
-      title: "Enjoy on your TV.",
-      subtitle:
-        "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
-      img: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png",
-    },
-    {
-      id: 2,
-      title: "Download your shows to watch offline.",
-      subtitle:
-        "Save your favourites easily and always have something to watch.",
-      img: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg",
-      reverse: true,
-    },
-    {
-      id: 3,
-      title: "Watch everywhere.",
-      subtitle:
-        "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
-      img: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png",
-    },
-  ];
+  const [features, setFeatures] = useState([]);
+
+  useEffect(() => {
+
+    const fetchFeatures = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/features");
+        setFeatures(response.data);
+      } catch (error) {
+        console.error("Error fetching features:", error);
+      }
+    };
+
+    fetchFeatures();
+  }, []);
 
   return (
     <div className="info-section">
-        <h1 className="Text">MORE REASON TO JOIN</h1>
+      <h1 className="Text">MORE REASON TO JOIN</h1>
       {features.map((f) => (
         <FeatureCard key={f.id} {...f} />
       ))}
     </div>
   );
 }
-    
